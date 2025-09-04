b9a614b344fe1becd099285da0e4dafed4500b8c
Alexandre <103534039+AlexSakai@users.noreply.github.com>
2025-09-04 17:38:33 -0300

    Vetores e Matrizes
    04/09

--- aula 8.cpp
+++ aula 8.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-
+/*
 //Variáveis
 int qtd;
 
@@ -22,4 +22,37 @@ int main(){
 		printf("\n%d", notas[i]);
 	}
 }
+*/
 
+//Variáveis
+
+
+int main(){	
+	setlocale(LC_ALL, "portuguese");
+	
+	int L, C;
+	
+	printf("Digite a quantidade de linhas da matriz: ");
+	scanf("%i", &L);
+	
+	printf("Digite a quantidade de colunas da matriz: ");
+	scanf("%d", &C);
+	
+	int mat [L][C], valor;
+	
+	printf("Digite muitos valores: ");
+	for(int i = 0; i < 	L; i++){
+		printf("\n%da linha: \n", i+1);
+		for(int j = 0; j < L; j++){
+			scanf("%d", &mat[i][j]);
+		}
+	}
+	
+	printf("\nSua matriz é: \n");
+	for(int i = 0; i < C; i++){
+		for(int j = 0; j < C; j++){
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");
+	}
+}
